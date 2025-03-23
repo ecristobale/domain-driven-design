b370557e075dcabec2a6c42fdb9f552490de6442
CEED\Ceed <eduardo.cristobalenriquez0388@gmail.com>
2025-03-23 19:08:29 +0100

    Created spring boot app, read Requirements.txt

---
+++ domain-driven-design/src/main/java/com/ecristobale/domaindrivendesign/DomainDrivenDesignExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.ecristobale.domaindrivendesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainDrivenDesignExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainDrivenDesignExerciseApplication.class, args);
+	}
+
+}
---
+++ domain-driven-design/src/test/java/com/ecristobale/domaindrivendesign/DomainDrivenDesignExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecristobale.domaindrivendesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainDrivenDesignExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
